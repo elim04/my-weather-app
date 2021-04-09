44407b7ad6471ac87cf584d3f57b6229aa80f1de
Emily Lim <emily.elim04@gmail.com>
2021-04-09 16:19:00 -0400

    add weatherItem and form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+import WeatherItem from "./components/WeatherItem";
+import LocationForm from "./components/LocationForm";
+
 function App() {
-  return <div className="App">My Weather App</div>;
+  const [city, setCity] = useState();
+  const [weatherData, setWeatherData] = useState();
+
+  useEffect(() => {
+    if (city) {
+      axios
+        .get(
+          `${process.env.REACT_APP_API_URL}/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+        )
+        .then((res) => {
+          setWeatherData(res.data);
+          console.log("success!", res.data);
+        })
+        .catch((err) => console.error(err));
+    }
+  }, [city]);
+
+  const getCityInfo = (formInfo) => {
+    setCity(formInfo);
+  };
+
+  return (
+    <div className="App">
+      {weatherData && <WeatherItem weatherData={weatherData} />}
+      <LocationForm getCityInfo={getCityInfo} />
+    </div>
+  );
 }
 
 export default App;
